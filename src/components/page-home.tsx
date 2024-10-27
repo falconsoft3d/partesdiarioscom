b35e72596c755e3f5de888d0a78849d040faa7fb
Marlon Falcón Hernández <falconsoft.3d@gmail.com>
2024-10-27 20:36:31 +0100

    sc

--- src/components/page-home.tsx
+++ src/components/page-home.tsx
@@ -1,6 +1,6 @@
 import { Card, CardContent } from "@/components/ui/card"
 import { Button } from "@/components/ui/button"
-import { Settings, FileText, ExternalLink, ArrowDown, ArrowUp } from "lucide-react"
+import { Settings, ExternalLink, ArrowDown, ArrowUp } from "lucide-react"
 import Link from "next/link"
 
 export default function Pagehome() {
