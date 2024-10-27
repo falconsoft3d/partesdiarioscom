1bc656701e52d56b2d3a9ee596c49ea290c6a237
Marlon Falcón Hernández <falconsoft.3d@gmail.com>
2024-10-27 15:03:22 +0100

    sc

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <div>
         <h1 className="text-2xl sm:text-4xl font-bold text-center sm:text-left">Partes Diarios</h1>
-        <p>Lleva el control de tus partes diarios de la empresa.</p>
+        <p>Lleva el control de tus partes diarios de la empresa. V20241027</p>
       </div>
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
@@ -38,14 +38,6 @@ export default function Home() {
               height={20}
             />
             Configurar Ahora
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Leer Documentación
           </a>
         </div>
       </main>
