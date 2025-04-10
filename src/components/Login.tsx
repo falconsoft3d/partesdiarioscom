70c785d3e7ec09a12ae606e0d3119298fe6a7e0f
Luis Mario Mora Vila <59125773+lmorav92@users.noreply.github.com>
2025-04-10 16:32:59 -0400

    Eliminar rules
    Tenporal

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 'use client';
 
 import { Grid, Paper, Typography, Button,  TextField, CircularProgress, InputAdornment, IconButton } from '@mui/material';
